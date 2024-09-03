d034e4fe401ce2884d7392dcc2e22246059992c5
David Villarreal-Zegarra <134119914+dvillarrealzegarra@users.noreply.github.com>
2024-09-02 22:48:27 -0600

    Update Assignment-Simple_Data_Types.py

--- Assignment-Simple_Data_Types.py
+++ Assignment-Simple_Data_Types.py
@@ -4,7 +4,7 @@
 # Jack has been prescribed 5 medications, write a variable named "Medication", and store in it the number of medications he has. Print the type of the variable ''Medication". Submit your code either here as a text entry, or on your GitHub page in a new repository and past the link here. We encourage you to use GitHub. Don't forget to add comments to your code.
 
 
-#Reply:
+# Reply:
 # Medication: number of medications Jack has been prescribed
 Medication = 5  
 
@@ -13,6 +13,28 @@ print(type(Medication))
 
 #  Print with explanation about the variable type of “Medication”.
 print("The type of the variable 'Medication' is:", type(Medication))
+
+
+
+
+
+###############
+# Question 8
+###############
+# Susan has a weight of 60 kilograms and a height of 1.58 meters. Write Code to Calculate and Print Susan’s Body Mass Index as float. Note: Body Mass Index is a person’s weight in kilograms divided by the square of height in meters. Reference: https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.htmlLinks to an external site.. Submit your code either here as a text entry, or on your GitHub page in a new repository and past the link here. We encourage you to use GitHub. Don't forget to add comments to your code.
+
+# Reply:
+# Susan's values
+weight = 60
+height = 1.58
+
+# Susan's Body Mass Index
+BMI = weight / (height ** 2)
+
+# Print Susan's BMI as a float
+print("Susan's Body Mass Index (BMI) is:", BMI)
+
+
 
 
 
