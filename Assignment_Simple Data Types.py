96a25dd5cca086300793592937b3e16d23b32b02
David Villarreal-Zegarra <134119914+dvillarrealzegarra@users.noreply.github.com>
2024-09-02 22:40:41 -0600

    Create Assignment_Simple Data Types.py

---
+++ Assignment_Simple Data Types.py
@@ -0,0 +1,20 @@
+###############
+# Question 7
+###############
+# Jack has been prescribed 5 medications, write a variable named "Medication", and store in it the number of medications he has. Print the type of the variable ''Medication". Submit your code either here as a text entry, or on your GitHub page in a new repository and past the link here. We encourage you to use GitHub. Don't forget to add comments to your code.
+
+
+#Reply:
+# Medication: number of medications Jack has been prescribed
+Medication = 5  
+
+# Print of "Medication"
+print(type(Medication))
+
+#  Print with explanation about the variable type of “Medication”.
+print("The type of the variable 'Medication' is:", type(Medication))
+
+
+
+
+
